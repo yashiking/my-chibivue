2425af30b85862f5e2373ca6d752b67e42fce77c
yashiking <jam.tennis57@gmail.com>
2024-11-22 01:42:16 +0900

    minimum virtual dom (pre)

--- packages/runtime-core/h.ts
+++ packages/runtime-core/h.ts
@@ -1,9 +1,9 @@
-import {VNode, VNodeProps} from "./vnode";
+import {createVNode, VNode, VNodeProps} from "./vnode";
 
 export function h(
   type: string,
   props: VNodeProps,
   children: (VNode | string)[],
 ) {
-  return { type, props, children }
+  return createVNode(type, props, children)
 }
--- packages/runtime-core/vnode.ts
+++ packages/runtime-core/vnode.ts
@@ -1,9 +1,37 @@
-export interface VNode {
-  type: string
-  props: VNodeProps
-  children: (VNode | string)[]
+export const Text = Symbol();
+
+export type VNodeTypes = string | typeof Text;
+
+export interface VNode<HostNode = any> {
+  type: VNodeTypes;
+  props: VNodeProps | null;
+  children: VNodeNormalizedChildren;
 }
 
 export interface VNodeProps {
   [key: string]: any
+}
+
+export type VNodeNormalizedChildren = string | VNodeArrayChildren;
+export type VNodeArrayChildren = Array<VNodeArrayChildren | VNodeChildAtom>;
+
+export type VNodeChild = VNodeChildAtom | VNodeArrayChildren;
+type VNodeChildAtom = VNode | string;
+
+export function createVNode(
+  type: VNodeTypes,
+  props: VNodeProps | null,
+  children: unknown,
+): VNode {
+  const vnode: VNode = { type, props, children }
+  return vnode
+}
+
+export function normalizeVNode(child: VNodeChild): VNode {
+  if (typeof child === "object") {
+    return { ...child } as VNode;
+  } else {
+    // stringだった場合に先ほど紹介した扱いたい形に変換する
+    return createVNode(Text, null, String(child));
+  }
 }
